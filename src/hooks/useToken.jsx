5ab60dbc085fc1da88632de5754ae257a6d2da48
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-05-02 14:00:52 +0600

    useToken hooks added

--- src/hooks/useToken.jsx
+++ src/hooks/useToken.jsx
@@ -3,11 +3,13 @@ import { useEffect, useState } from "react"
 const useToken = (user) => {
     const [token,setToken] = useState('')
     useEffect(() => {
-        const getToken =async () => {
+        const getToken = async () => {
             const email = user?.user?.email
-            const {data} = await axios.post('http://localhost:5000/login',{email})
-            setToken(data?.accessToken)
-            localStorage.setItem('accessToken',data?.accessToken)
+            if(email){
+                const {data} = await axios.post('http://localhost:5000/login',{email})
+                setToken(data?.accessToken)
+                localStorage.setItem('accessToken',data?.accessToken)
+            }
         }
         getToken()
     },[user])
