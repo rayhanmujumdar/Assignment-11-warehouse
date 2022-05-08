4ae86550fbf2c0b5e17e39e78ebd3b9adac757cd
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-05-08 18:47:23 +0600

    banner section updated

--- src/Components/Pages/Banner/Banner.jsx
+++ src/Components/Pages/Banner/Banner.jsx
@@ -18,13 +18,13 @@ const Banner = () => {
         {bannerItems.map((item) => (
           <div
             key={item._id}
-            className="item h-[92vh] bg-cover bg-center flex"
+            className="item md:h-screen bg-cover bg-center flex"
             style={{ backgroundImage: `url(${backgroundImage})` }}
           >
             <div className="grid md:grid-cols-2 justify-items-center items-center gap-10 container mx-auto">
               <div className="md:order-1 order-2 p-4 md:text-left text-center">
                 <h1 className="text-3xl uppercase my-2 text-slate-600 text font-semibold">{item?.name}</h1>
-                <p className="my-3 font-thin text-lg">{item?.description}</p>
+                <p className="my-3 font-thin text-lg">{item?.description.slice(0,200)}</p>
                 <p className="text-xl text-gray-700">Price: {item?.price} Taka</p>
               </div>
               <div 
